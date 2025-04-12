831f7939a4e39bcbd43f6d57d0def7fd0945d261
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2025-04-12 18:32:25 +0100

    Refactor Header component for improved layout consistency and responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,17 @@ export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between px-[20px] py-[20px] relative lg:px-[40px]">
-        <h1 className="font-[Mulish] font-bold text-[18px] leading-none mr-[300px]">Travelsy</h1>
-        <div className="lg:hidden">
+        <h1 className="font-[Mulish] font-bold text-[18px] leading-none ">Travelsy</h1>
+        <div className="lg:hidden flex text-center gap-2">
           <button onClick={() => setMenuOpen(!menuOpen)} className="text-xl text-[#4A4A4A]">
             {menuOpen ? <FaTimes /> : <FaBars />}
           </button>
+          <FaSearch className="text-[#4A4A4A] mt-2" />
+            <div className="relative inline-block">
+              <img src={oval} alt="oval" className="border border-white rounded-full w-8 h-8" />
+              <span className="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 border border-white rounded-full animate-pulse"></span>
+            </div>
+            <FaChevronDown className="text-[#4A4A4A] mt-2" />
         </div>
         <ul className={`
           ${menuOpen ? 'flex' : 'hidden'}
@@ -24,7 +30,15 @@ export default function Header() {
           <li><p>Activities</p></li>
           <li><p>Equipment</p></li>
           <li><p>Blogs</p></li>
+
+          <li className="lg:hidden">
+            <div className="flex items-center text-white font-[Mulish] text-[13px] border rounded-3xl gap-1 px-6 py-[6px] bg-[#FF5200]">
+              <FaBookmark />
+              <p>Reservation</p>
+            </div>
+          </li>
         </ul>
+
         <div className="hidden lg:flex items-center gap-[12px] pr-[20px] lg:pr-[60px]">
           <div className="flex items-center text-white font-[Mulish] text-[13px] border rounded-3xl gap-1 px-6 py-[3px] bg-[#FF5200]">
             <FaBookmark />
